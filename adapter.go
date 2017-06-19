a7f797bd12d88c47e05f981672baff2fcc4f1a4d
ankit <ankit@gooddoglabs.com>
2017-06-19 14:15:59 -0400

    Write Policy to Consul

--- adapter.go
+++ adapter.go
@@ -2,6 +2,7 @@ package ConsulAdapter
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/inconshreveable/log15"
 
@@ -41,13 +42,28 @@ func (a *DBAdapter) Get(key string) (*api.KVPair, *api.QueryMeta, error) {
 	return a.kv.Get(key, nil)
 }
 
+// func loadPolicyLine(line string, model model.Model) {
+// 	if line == "" {
+// 		return
+// 	}
+
+// 	tokens := strings.Split(line, ", ")
+
+// 	key := tokens[0]
+// 	fmt.Println("key: ", key)
+// 	sec := key[:1]
+// 	fmt.Println("sec: ", sec)
+// 	model[sec][key].Policy = append(model[sec][key].Policy, tokens[1:])
+// 	fmt.Println(model)
+// }
+
 // LoadPolicy loads policy from database.
 func (a *DBAdapter) LoadPolicy(model model.Model) {
 	a.init()
 
 	for ptype, ast := range model["p"] {
 		for _, rule := range ast.Policy {
-			fmt.Println("Load Policy: ", ptype, "  ", rule)
+			fmt.Println(ptype, " ", rule)
 		}
 	}
 
@@ -58,19 +74,38 @@ func (a *DBAdapter) LoadPolicy(model model.Model) {
 	}
 }
 
+func (a *DBAdapter) writePolicyLine(ptype string, rule []string, idx int) {
+	line := "'" + ptype + "'"
+	fmt.Println(line)
+	for i := range rule {
+		line += ",'" + rule[i] + "'"
+	}
+	for i := 0; i < 4-len(rule); i++ {
+		line += ",''"
+	}
+	fmt.Println(idx)
+	p := &api.KVPair{Key: ptype + strconv.Itoa(idx), Value: []byte(line)}
+	success, _, err := a.kv.CAS(p, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("CAS operation: ", success)
+
+}
+
 // SavePolicy saves policy to database.
 func (a *DBAdapter) SavePolicy(model model.Model) {
 	a.init()
 
 	for ptype, ast := range model["p"] {
-		for _, rule := range ast.Policy {
-			fmt.Println(ptype, "  ", rule)
+		for idx, rule := range ast.Policy {
+			a.writePolicyLine(ptype, rule, idx)
 		}
 	}
 
 	for ptype, ast := range model["g"] {
-		for _, rule := range ast.Policy {
-			fmt.Println("Load Policy: ", ptype, "  ", rule)
+		for idx, rule := range ast.Policy {
+			a.writePolicyLine(ptype, rule, idx)
 		}
 	}
 }
