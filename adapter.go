682beff78c5e3bffa70297e4b90148232634cf23
ankit <ankit@gooddoglabs.com>
2017-06-28 06:47:23 -0400

    Store casbin policies and group role relation in one consul key, helps in editing and deleting policies

--- adapter.go
+++ adapter.go
@@ -1,13 +1,12 @@
 package consuladapter
 
 import (
+	"encoding/json"
 	"errors"
-	"strconv"
 	"strings"
 
 	"github.com/casbin/casbin/model"
 	"github.com/hashicorp/consul/api"
-	"github.com/inconshreveable/log15"
 )
 
 // KVAdapter represents the consul adapter for policy persistence, can load policy from consul or save policy to consul.
@@ -33,86 +32,91 @@ func (a *KVAdapter) list(prefix string) (api.KVPairs, *api.QueryMeta, error) {
 	return a.kv.List(prefix, nil)
 }
 
-func loadPolicyLine(line string, model model.Model) {
+func loadPolicyKey(line string, model model.Model) {
 	if line == "" {
 		return
 	}
-	tokens := strings.Split(line, ", ")
+
+	tokens := strings.Split(line, ";")
 	key := tokens[0]
 	sec := key[:1]
 	model[sec][key].Policy = append(model[sec][key].Policy, tokens[1:])
+
 }
 
 // LoadPolicy loads policy from consul.
-func (a *KVAdapter) LoadPolicy(model model.Model) {
+func (a *KVAdapter) LoadPolicy(model model.Model) error {
+	line := [][]string{}
+	//rule := ""
 
-	pairs, _, err := a.list("rp")
+	//TODO: Write a get function
+	pair, _, err := a.kv.Get("rp", nil)
 	if err != nil {
-		log15.Error("Could not retreive list of key-value pairs", "Error", err)
-		//return err
-	}
-	for _, v := range pairs {
-		line := string(v.Value)
-		loadPolicyLine(line, model)
-	}
-
-	if err != nil {
-
-	}
-	//return nil
-}
-
-func (a *KVAdapter) writePolicyLine(ptype string, rule []string) error {
-	line := ptype
-
-	for i := range rule {
-		line += ", " + rule[i]
-	}
-	// for i := 0; i < 4-len(rule); i++ {
-	// 	line += ","
-	// }
-	_, meta, err := a.list("rp")
-	if err != nil {
-		log15.Error("Could not retrieve key-value pair", "Error", err)
 		return err
 	}
-	p := &api.KVPair{Key: ptype + strconv.FormatUint(meta.LastIndex, 10), Value: []byte(line)}
+	if pair != nil {
+		json.Unmarshal(pair.Value, &line)
+
+		for _, v := range line {
+			if len(v) > 2 {
+				v = append([]string{"p"}, v...)
+			} else {
+				v = append([]string{"g"}, v...)
+			}
+
+			rule := strings.Join(v, ";")
+			loadPolicyKey(rule, model)
+
+		}
+
+	}
+
+	return nil
+}
+
+func (a *KVAdapter) writePolicyKey(rule [][]string) error {
+	pair, _, err := a.kv.Get("rp", nil)
+	if err != nil {
+		return err
+	}
+
+	value, _ := json.Marshal(rule)
+
+	p := &api.KVPair{Key: "rp", Value: []byte(value)}
+
+	//If not set, the default value is 0, and CAS will fail
+	if pair != nil {
+		p.ModifyIndex = pair.ModifyIndex
+	}
+
 	if success, _, err := a.cas(p); success {
 		if err != nil {
-			log15.Error("Check and Set failed for Consul KV", "Error", err)
 			return err
 		}
 	} else {
 		err = errors.New("Check and set returned false for Consul KV")
-		log15.Error("Check and Set returned false for Consul KV", "Error", err)
 		return err
 	}
 	return nil
 }
 
 // SavePolicy saves policy to consul.
-func (a *KVAdapter) SavePolicy(model model.Model) {
+func (a *KVAdapter) SavePolicy(model model.Model) error {
 
-	//Loop over the policies
-	for ptype, ast := range model["p"] {
-		for _, rule := range ast.Policy {
-			err := a.writePolicyLine(ptype, rule)
-			if err != nil {
-				log15.Error("Error storing policy to consul KV store", "Error", err)
-				//return err
-			}
-		}
+	var rule [][]string
+	if len(model["p"]["p"].Policy) != 0 {
+
+		rule = append(model["p"]["p"].Policy, rule...)
+		a.writePolicyKey(rule)
+	}
+	if len(model["g"]["g"].Policy) != 0 {
+
+		rule = append(model["g"]["g"].Policy, rule...)
+		a.writePolicyKey(rule)
+	}
+	if len(rule) == 0 {
+		return errors.New("Invalid policy (policy cannot be empty)")
 	}
 
-	//Loop over group to role mapping
-	for ptype, ast := range model["g"] {
-		for _, rule := range ast.Policy {
-			err := a.writePolicyLine(ptype, rule)
-			if err != nil {
-				log15.Error("Error storing policy to consul KV store", "Error", err)
-				//return err
-			}
-		}
-	}
-	//return nil
+	return nil
 }
